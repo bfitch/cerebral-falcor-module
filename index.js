099594ee1c789da7ffcfc76a0c5a51535a0ddde8
Brian Fitch <brianmfitch@gmail.com>
2016-01-14 23:28:12 -0500

    Basic working version

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ import HTTPDataSource from 'falcor-http-datasource';
 import expandCache from 'falcor-expand-cache';
 import deepDiff from 'deep-diff';
 
-export default (options = {})=> {
+export default (options = {}) => {
   return (module) => {
     module.alias('cerebral-module-falcor');
 
     const fullOptions = Object.assign({
       initialState: {},
       dataSource:   '/model.json',
-      verbose:      true
+      verbose:      false
     }, options);
     const {initialState, dataSource, verbose} = fullOptions;
 
     module.state(initialState);
 
     // Calculate falcor changes and apply them to the module's scoped state
-    const mergeFalcorWithModel = [
+    const syncCerebral = [
       ({module})=> {
         const falcorCache         = falcorModel.getCache();
         const expandedFalcorCache = expandCache(falcorCache);
@@ -43,7 +43,7 @@ export default (options = {})=> {
                 try {
                   module.state.merge(path, rhs);
                 }
-                catch(e){
+                catch(e) {
                   // In some cases you can't merge (converting from a bool to object and should set the new value
                   module.state.set(path,rhs);
                 }
@@ -59,7 +59,7 @@ export default (options = {})=> {
       }
     ];
 
-    module.signalsSync({mergeFalcorWithModel});
+    module.signalsSync({syncCerebral});
 
     const source      = new HTTPDataSource(dataSource);
     const falcorModel = new falcor.Model({source});
@@ -71,24 +71,28 @@ export default (options = {})=> {
       set(jsonGraph){
         return falcorModel.set(jsonGraph);
       },
-      call(callPath, args) {
-        const jsonArgs = (typeof args == 'string') ? args : JSON.stringify(args);
-        return falcorModel.call(callPath, jsonArgs);
+      call(functionPath, args = [], refSuffixes = [], thisPaths = []) {
+        // functionPath - {Path}            the path to the function to invoke
+        // args         - {Array.<Object>}  the arguments to pass to the function
+        // refSuffixes  - {Array.<PathSet>} paths to retrieve from the targets of JSONGraph References in the function's response.
+        // thisPaths    - {Array.<PathSet>} paths to retrieve from function's this object after successful function execution
+        return falcorModel.call(functionPath, args, refSuffixes, thisPaths);
       }
     });
 
     // call merge signal on falcor changes
-    falcorModel._root.onChange = module.getSignals().mergeFalcorWithModel;
+    falcorModel._root.onChange = module.getSignals().syncCerebral;
 
     // meta
     return {warning: 'Falcor module is super experimental, use at your own risk!'};
   }
 }
 
-export function falcorGet(path, {debug = true, verbose = true}) {
-  return async ({output,module})=> {
+export function falcorGet(path, {debug = false, verbose = false} = {}) {
+  return async ({output, module}) => {
     try {
       const fullPath = Array.isArray(path) ? path : [path];
+
       const response = await module['cerebral-module-falcor'].services.get(fullPath);
       output(response.json);
     }
@@ -100,13 +104,20 @@ export function falcorGet(path, {debug = true, verbose = true}) {
   }
 }
 
-export function falcorCall(path, {debug = true, verbose = true}) {
-  return async ({input,output,module})=> {
+export function falcorCall(path, {debug = false, verbose = false} = {}) {
+  return async ({input, output, module}) => {
     try {
-      const {falcorArgs} = input;
+      const {falcorArgs, falcorRefSuffixes = [], falcorThisPaths = []} = input;
       if (!falcorArgs) throw new Error(`Must have a falcorArgs object in the input or a call can't be made to Falcor.`);
       const fullPath = Array.isArray(path) ? path : [path];
-      const response = await module['cerebral-module-falcor'].services.call(fullPath, falcorArgs);
+
+      const response = await module['cerebral-module-falcor'].services.call(
+        fullPath,
+        falcorArgs,
+        falcorRefSuffixes,
+        falcorThisPaths
+      );
+
       output(response.json);
     }
     catch (e) {
